089480ed605cfe53caa1f337b0e2212a9398a844
Lancaster Wu <wuj@allegheny.edu>
2020-04-24 10:21:56 -0400

    finished setup

---
+++ README.md
@@ -0,0 +1,14 @@
+# Generalized-Fault-Localization
+A pytest plugin which runs SBFL algorithms to detect faults.
+
+First five algorithms are based on what Spencer Pearson, José Campos, René Just, Gordon Fraser, Rui Abreu, Michael D. Ernst, Deric Pang, Benjamin Keller discribed in [Evaluating and improving fault localization](https://homes.cs.washington.edu/~rjust/publ/fault_localization_effectiveness_icse_2017.pdf).
+
+#### reference
+
+Pearson, S., Campos, J., Just, R., Fraser, G., Abreu, R., Ernst, M. D., ... & Keller, B. (2017, May). Evaluating and improving fault localization. In *2017 IEEE/ACM 39th International Conference on Software Engineering (ICSE)* (pp. 609-620). IEEE.
+
+#### required installation
+
+`pip install git+https://github.com/nedbat/pytest-cov.git@nedbat/contexts`
+
+`pip install coverage>=5.1`
---
+++ setup.py
@@ -0,0 +1,30 @@
+"""This file is used to deploy the plugin"""
+import io
+import os
+from setuptools import setup
+
+install_requires = ["coverage>=5.1", "pytest>=4.4.0", "pytest-cov"]
+
+
+def read(filename):
+    """This function takes in a filepath and reads the file"""
+    filepath = os.path.join(os.path.dirname(__file__), filename)
+    with io.open(filepath, mode="r", encoding="utf-8") as f:
+        return f.read()
+
+
+setup(
+    name="pytest-pinpoint",
+    version="0.0.3",
+    description="A pytest plugin which runs SBFL algorithms to detect faults.",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Lancaster Wu",
+    author_email="wuj@allegheny.edu",
+    url="https://github.com/Generalized-SBFL/pytest-pinpoint",
+    license="GNU",
+    platforms="any",
+    install_requires=install_requires,
+    py_modules=["pytest_pinpoint"],
+    entry_points={"pytest11": ["pinpoint = pytest_pinpoint"]},
+)
